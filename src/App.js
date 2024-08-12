0284ed168a076acc115c6b59c29a85abf8d7dc97
gamingwizx <gamingwizx@gmail.com>
2024-08-13 05:00:58 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useRef, useState } from "react";
+import { motion } from "framer-motion";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ItemContainer key="container" />
     </div>
+  );
+}
+function ItemContainer() {
+  const draggedId = useRef(0);
+  const enteredId = useRef(0);
+  const [items, setItems] = useState([
+    { id: 1, name: "Item 1" },
+    { id: 2, name: "Item 2" },
+    { id: 3, name: "Item 3" },
+  ]);
+  const handleDrop = () => {
+    const copy = items.slice().filter((item) => item.id !== draggedId.current);
+    const draggedItem = items
+      .slice()
+      .find((item) => item.id === draggedId.current);
+    const enteredIndex = items
+      .slice()
+      .findIndex((item) => item.id === enteredId.current);
+    copy.splice(enteredIndex, 0, draggedItem);
+    setItems(copy);
+  };
+  return (
+    <div
+      onDragOver={(e) => e.preventDefault()}
+      onDrop={() => handleDrop()}
+      className="item-container"
+    >
+      {items.map((item) => (
+        <Item
+          key={item.id}
+          draggedId={draggedId}
+          enteredId={enteredId}
+          item={item}
+        />
+      ))}
+    </div>
+  );
+}
+function Item({ item, draggedId, enteredId }) {
+  return (
+    <motion.div
+      layout
+      layoutId={item.id}
+      onDragStart={(e) => (draggedId.current = item.id)}
+      onDragEnter={(e) => (enteredId.current = item.id)}
+      draggable="true"
+      className="item"
+    >
+      {item.name}
+    </motion.div>
   );
 }
 
